b1c617971ecf5c8e1dbd3bfcbe0a22ee2e26785b
Andrey Usanov <iambreaddd@mail.ru>
2021-03-12 10:28:06 +0300

    Add more tests for tree

--- lab4/main.cpp
+++ lab4/main.cpp
@@ -3,27 +3,38 @@
 
 int main()
 {
-  BinarySearchTree<int> a;
-  a.addIterative(10);
-  a.addIterative(5);
-  a.addIterative(12);
-  a.addIterative(3);
-  a.addIterative(7);
-  a.addIterative(2);
-  a.addIterative(6);
-  a.addIterative(8);
+  BinarySearchTree<int> myBinaryTree;
+  myBinaryTree.addIterative(10);
+  myBinaryTree.addIterative(5);
+  myBinaryTree.addIterative(12);
+  myBinaryTree.addIterative(3);
+  myBinaryTree.addIterative(7);
+  myBinaryTree.addIterative(2);
+  myBinaryTree.addIterative(6);
+  myBinaryTree.addIterative(8);
   std::cout << "Created tree with elements 10,5,12,3,8,7,6,2.\n";
-  a.printInfo(3);
-  if (a.findElement(7))
+  myBinaryTree.printInfo(3);
+  if (myBinaryTree.findElement(7))
   {
     std::cout << "Element with value 7 was found\n";
   }
-  std::cout << "Number of nodes is " << a.numberOfNodes() << "\n";
-  std::cout << "Height of tree is " << a.height() << "\n";
-  a.removeElement(5);
+  std::cout << "Number of nodes is " << myBinaryTree.numberOfNodes() << "\n";
+  std::cout << "Height of tree is " << myBinaryTree.height() << "\n";
+  myBinaryTree.removeElement(5);
   std::cout << "Checking third node after deleting node with key 5: \n";
-  a.printInfo(3);
-  a.deleteTreeRecursive();
+  myBinaryTree.printInfo(3);
+  myBinaryTree.deleteTreeRecursive();
+
+  std::cout << "\nCreating a tree with chars 'g','a','k'.\n";
+  BinarySearchTree<char> charTree;
+  charTree.addRecursive('g');
+  charTree.addRecursive('a');
+  charTree.addRecursive('k');
+  std::cout << "Number of nodes is " << charTree.numberOfNodes() << "\n";
+  if(charTree.findElement('g')) std::cout << "Node 'g' found.\n";
+  if(charTree.findElement('a')) std::cout << "Node 'a' found.\n";
+  if(charTree.findElement('k')) std::cout << "Node 'k' found.\n";
+
   return 0;
 }
 
