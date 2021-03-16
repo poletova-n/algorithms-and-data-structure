550cd6c583cb26e2462d108f7385a14cf34a50bd
vladlukynov <lukynov.1@yandex.ru>
2021-03-16 12:10:12 +0300

    Add -= operator

--- lab1/DoubleLinkedList.hpp
+++ lab1/DoubleLinkedList.hpp
@@ -5,32 +5,34 @@
 class DoubleLinkedList
 {
 public:
-	DoubleLinkedList();
-	DoubleLinkedList(const DoubleLinkedList& object);
-	void operator += (int data);
-	void operator = (const DoubleLinkedList& object);
-	int operator [] (int size);
-	bool operator == (DoubleLinkedList& object);
-	friend DoubleLinkedList operator & (DoubleLinkedList& object1, DoubleLinkedList& object2);
-	friend DoubleLinkedList operator | (DoubleLinkedList& object1, DoubleLinkedList& object2);
-	friend std::ostream& operator<<(std::ostream& output, DoubleLinkedList& object);
-	void merge(DoubleLinkedList& object);
-	~DoubleLinkedList();
+  DoubleLinkedList();
+  DoubleLinkedList(const DoubleLinkedList &object);
+  void operator+=(int data);
+  void operator-=(int data);
+  void operator=(const DoubleLinkedList &object);
+  int operator[](int size);
+  bool operator==(DoubleLinkedList &object);
+  friend DoubleLinkedList operator&(DoubleLinkedList &object1, DoubleLinkedList &object2);
+  friend DoubleLinkedList operator|(DoubleLinkedList &object1, DoubleLinkedList &object2);
+  friend std::ostream &operator<<(std::ostream &output, DoubleLinkedList &object);
+  void merge(DoubleLinkedList &object);
+  void print();
+  ~DoubleLinkedList();
 private:
-	class Node
-	{
-	public:
-		Node(int data, Node* next = nullptr, Node* prev = nullptr)
-		{
-			prev_ = prev;
-			next_ = next;
-			data_ = data;
-		}
-		Node* prev_;
-		Node* next_;
-		int data_;
-	};
-	Node* head_;
-	int count_;
+  class Node
+  {
+  public:
+    Node(int data, Node *next = nullptr, Node *prev = nullptr)
+    {
+      prev_ = prev;
+      next_ = next;
+      data_ = data;
+    }
+    Node *prev_;
+    Node *next_;
+    int data_;
+  };
+  Node *head_;
+  int count_;
 };
 #endif
