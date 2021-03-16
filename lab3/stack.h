406c33662090ad1d5e5214827ab3b6510e3d24b9
vladlukynov <lukynov.1@yandex.ru>
2021-03-16 06:58:57 +0300

    Add lab3 files

---
+++ lab3/stack.h
@@ -0,0 +1,16 @@
+#ifndef STACK_H
+#define STACK_H
+
+template<class T>
+class Stack
+{
+public:
+  virtual ~Stack() = default;
+  virtual void push(const T &element) = 0;
+  virtual T pop() = 0;
+  virtual bool isEmpty() = 0;
+  virtual T getTop() = 0;
+  virtual void deleteTop() = 0;
+};
+
+#endif
