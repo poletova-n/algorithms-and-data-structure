1976429d9e562d499134656f95db4b473d11fa75
Dmitry_Bulatov <bulatovdima1@gmail.com>
2021-03-17 19:01:44 +0300

    Fix bugs with brackets

--- lab3/main.cpp
+++ lab3/main.cpp
@@ -4,22 +4,24 @@
 #include "operator.hpp"
 
 bool isDigit(const char& t);
-std::string translate(const std::string& exp);
+std::string translate(const std::string& exp, bool n);
+std::string infToPost(const std::string &exp);
 int calculation(const std::string& text);
+bool testBalanceBrackets(const std::string& exp, int maxDeep);
 
 int main()
 {
   try
   {
-    std::string test1 = "5+1";
-    std::cout << "Test1: " << test1 << " = " << translate(test1) << " = "
-              << calculation(translate(test1)) << "\n";
-    std::string test2 = "9-4+2*3";
-    std::cout << "Test2: " << test2 << " = " << translate(test2) << " = "
-              << calculation(translate(test2)) << "\n";
+    std::string test1 = "(5+1)";
+    std::cout << "Test1: " << test1 << " = " << infToPost(test1) << " = "
+              << calculation(translate(test1, 1)) << "\n";
+    std::string test2 = "9-4+(2*3)";
+    std::cout << "Test2: " << test2 << " = " << infToPost(test2) << " = "
+              << calculation(translate(test2, 1)) << "\n";
     std::string test3 = "3/3-4*2+8";
-    std::cout << "Test3: " << test3 << " = " << translate(test3) << " = "
-              << calculation(translate(test3)) << "\n";
+    std::cout << "Test3: " << test3 << " = " << infToPost(test3) << " = "
+              << calculation(translate(test3, 1)) << "\n";
   }
   catch(const std::logic_error &e)
   {
@@ -37,7 +39,14 @@ bool isDigit(const char& t)
   return false;
 }
 
-std::string translate(const std::string &exp)
+std::string infToPost(const std::string &exp)
+{
+  if (!testBalanceBrackets(exp, 30))
+    throw std::logic_error("Not balanced");
+  return translate(exp, 0);
+}
+
+std::string translate(const std::string &exp, bool n)
 {
   std::string result = "";
   StackArray<char> stack(exp.size());
@@ -74,7 +83,8 @@ std::string translate(const std::string &exp)
         break;
       }
       case '(':
-        stack.push(exp[i]);
+        if (n == 0)
+          stack.push(exp[i]);
         break;
       case ')':
       {
@@ -84,6 +94,8 @@ std::string translate(const std::string &exp)
           last = stack.getTop();
           result += (stack.pop());
         }
+        if (n == 0)
+          stack.push(exp[i]);
         break;
       }
     }
@@ -138,4 +150,40 @@ int calculation(const std::string& exp)
     }
   }
   return stack.pop();
+}
+
+bool testBalanceBrackets(const std::string &exp, int maxDeep)
+{
+  Stack<char>* stack = new StackArray<char>(maxDeep);
+  bool isBalanceBrackets = true;
+  try
+  {
+    char cText = '\0';
+    for (int i = 0; ((cText = exp[i]) != '\0') && (isBalanceBrackets == true); i++)
+    {
+      switch (cText)
+      {
+        case '(':
+          stack->push(cText);
+          break;
+        case ')':
+          if (stack->pop() != '(')
+          {
+            isBalanceBrackets = false;
+          }
+          break;
+      }
+    }
+    isBalanceBrackets = isBalanceBrackets && stack->isEmpty();
+  }
+  catch (StackUnderflow)
+  {
+    isBalanceBrackets = false;
+  }
+  catch (StackOverflow)
+  {
+    isBalanceBrackets = false;
+  }
+  delete stack;
+  return isBalanceBrackets;
 }
