121ee9cef6dae9b0be587a14f5f35f438f895d13
sold666 <korch937@mail.ru>
2021-03-17 03:14:20 +0300

    Fix and rename function, add new

--- lab3/main.cpp
+++ lab3/main.cpp
@@ -5,26 +5,27 @@
 #include "StackArray.hpp"
 #include "Operator.hpp"
 
-std::string translationInfixExpressionToPostfix(const std::string& text);
+std::string transformInfixExpressionToPostfix(const std::string& text);
 int calculatePostfixForm(const std::string& text);
 void checkString(const std::string& text);
+bool arrayBalanceBrackets(const std::string& text);
 
 int main()
 {
   try
   {
-    std::string text1 = "(7 + 1) * 4";
-    std::cout << translationInfixExpressionToPostfix(text1) << " --> " << calculatePostfixForm(translationInfixExpressionToPostfix(text1)) << '\n';
+    std::string text1 = "7 + 1 * 4";
+    std::cout << transformInfixExpressionToPostfix(text1) << " --> " << calculatePostfixForm(transformInfixExpressionToPostfix(text1)) << '\n';
     std::string text2 = "(3 + 5) * ((1 + 2) - 4)";
-    std::cout << translationInfixExpressionToPostfix(text2) << " --> " << calculatePostfixForm(translationInfixExpressionToPostfix(text2)) << '\n';
+    std::cout << transformInfixExpressionToPostfix(text2) << " --> " << calculatePostfixForm(transformInfixExpressionToPostfix(text2)) << '\n';
     std::string text3 = "((5 + 5) * 4) / 1";
-    std::cout << translationInfixExpressionToPostfix(text3) << " --> " << calculatePostfixForm(translationInfixExpressionToPostfix(text3)) << '\n';
+    std::cout << transformInfixExpressionToPostfix(text3) << " --> " << calculatePostfixForm(transformInfixExpressionToPostfix(text3)) << '\n';
     std::string text4 = "((3 + 9) * 3) / (2 + 10)";
-    std::cout << translationInfixExpressionToPostfix(text4) << " --> " << calculatePostfixForm(translationInfixExpressionToPostfix(text4)) << '\n';
+    std::cout << transformInfixExpressionToPostfix(text4) << " --> " << calculatePostfixForm(transformInfixExpressionToPostfix(text4)) << '\n';
     std::string text5 = "((3 + 2) * 4) * (2 - 1)";
-    std::cout << translationInfixExpressionToPostfix(text5) << " --> " << calculatePostfixForm(translationInfixExpressionToPostfix(text5)) << '\n';
+    std::cout << transformInfixExpressionToPostfix(text5) << " --> " << calculatePostfixForm(transformInfixExpressionToPostfix(text5)) << '\n';
     std::string text6 = "((3 + 1) * 3) + ((3 + 1) + (7 - 1))";
-    std::cout << translationInfixExpressionToPostfix(text6) << " --> " << calculatePostfixForm(translationInfixExpressionToPostfix(text6)) << '\n';
+    std::cout << transformInfixExpressionToPostfix(text6) << " --> " << calculatePostfixForm(transformInfixExpressionToPostfix(text6)) << '\n';
   }
   catch(const std::invalid_argument &error)
   {
@@ -37,11 +38,16 @@ int main()
   return 0;
 }
 
-std::string translationInfixExpressionToPostfix(const std::string& text)
+std::string transformInfixExpressionToPostfix(const std::string& text)
 {
   StackArray<char> stack(text.size());
   std::string newExpression = "";
   checkString(text);
+  if(!(arrayBalanceBrackets(text)))
+  {
+    std::cout << text << '\n';
+    throw std::logic_error("Check the brackets!");
+  }
 
   for (int i = 0; i < text.size(); ++i)
   {
@@ -54,24 +60,46 @@ std::string translationInfixExpressionToPostfix(const std::string& text)
       newExpression += text[i];
       continue;
     }
-    else
+    else if (text[i] == '+' || text[i] == '-' || text[i] == '*' || text[i] == '/' || text[i] == '^')
     {
-      if (text[i] == ')')
-      {
-        while (stack.top() != '(')
-        {
-          newExpression += stack.pop();
-        }
-        if (stack.top() == '(')
-        {
-          stack.pop();
-        }
-        continue;
-      }
-      else if (Operator(text[i]).getPriority() != -1)
+      Operator currentOperand(text[i]);
+      Operator topOperand(stack.top());
+
+      if ((stack.isEmpty()) || (stack.top() == '('))
       {
         stack.push(text[i]);
       }
+      else if (currentOperand.getPriority() > topOperand.getPriority())
+      {
+        stack.push(text[i]);
+      }
+      else if (currentOperand.getPriority() <= topOperand.getPriority())
+      {
+        char temp = stack.top();
+        while ((temp != '(' || Operator(temp).getPriority() > Operator(stack.top()).getPriority()) && !(stack.isEmpty()))
+        {
+          temp = stack.top();
+          stack.push(stack.pop());
+        }
+        stack.push(text[i]);
+      }
+    }
+    else if (text[i] == '(')
+    {
+      stack.push(text[i]);
+      continue;
+    }
+    else if (text[i] == ')')
+    {
+      while (stack.top() != '(' && !(stack.isEmpty()))
+      {
+        newExpression += stack.pop();
+      }
+      if (stack.top() == '(')
+      {
+        stack.pop();
+      }
+      continue;
     }
   }
   while (!(stack.isEmpty()))
@@ -138,5 +166,77 @@ void checkString(const std::string& text)
     {
       throw std::invalid_argument("Invalid string specified.");
     }
+  }
+}
+
+bool arrayBalanceBrackets(const std::string& text)
+{
+  StackArray<char> stack(text.size());
+
+  try
+  {
+    for (int i = 0; i < text.size(); i++)
+    {
+      if ((text[i] == '(') || (text[i] == '[') || (text[i] == '{'))
+      {
+        stack.push(text[i]);
+        continue;
+      }
+      else if ((text[i] == ')') || (text[i] == ']') || (text[i] == '}'))
+      {
+        if (text[i] == ')')
+        {
+          if (stack.top() == '(' && (!stack.isEmpty()))
+          {
+            stack.pop();
+            continue;
+          }
+          else
+          {
+            return false;
+          }
+        }
+        if (text[i] == ']')
+        {
+          if (stack.top() == '[' && (!stack.isEmpty()))
+          {
+            stack.pop();
+            continue;
+          }
+          else
+          {
+            return false;
+          }
+        }
+        if (text[i] == '}')
+        {
+          if (stack.top() == '{' && (!stack.isEmpty()))
+          {
+            stack.pop();
+            continue;
+          }
+          else
+          {
+            return false;
+          }
+        }
+      }
+    }
+    if (stack.isEmpty())
+    {
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+  }
+  catch (StackUnderflow)
+  {
+    return false;
+  }
+  catch (StackOverflow)
+  {
+    return false;
   }
 }
