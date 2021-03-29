adca1aa8bb5b40466befebef13a2fdd33bb36e6a
Andrey Usanov <iambreaddd@mail.ru>
2021-03-29 17:55:38 +0300

    Add files of laboratory 5-6

--- lab5/main.cpp
+++ lab5/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "queuearray.hpp"
 #include "queuelist.hpp"
+#include "BinarySearchTree.hpp"
 
 int main()
 {
@@ -55,6 +56,40 @@ int main()
   {
     std::cout << secondQueueList.deQueue() << " ";
   }
+  std::cout << "\n\nTesting tree algorithms.\n";
+  BinarySearchTree<int> tree;
+  tree.addIterative(10);
+  tree.addIterative(5);
+  tree.addIterative(12);
+  tree.addIterative(3);
+  tree.addIterative(7);
+  tree.addIterative(2);
+  tree.addIterative(6);
+  tree.addIterative(8);
+  std::cout << "Created tree with nodes: 10 5 12 3 7 2 6 8\n";
+  std::cout << "Iterative walk in width of the tree: ";
+  tree.iterativeBreadth();
+  std::cout << "\n";
+  std::cout << "Iterative walk inorder(prefix form): ";
+  tree.iterativeInorder();
+  std::cout << "\nCopying the tree to a new one.\n";
+  BinarySearchTree<int> tree2(tree);
+  std::cout << "Are these trees same? ";
+  if(tree.sameTrees(tree2)) std::cout << "Yes";
+  else std::cout << "No";
+
+  std::cout << "\nCreating a new tree with nodes: 10 5 12 3 7 2 6\n";
+  BinarySearchTree<int> tree3;
+  tree3.addIterative(10);
+  tree3.addIterative(5);
+  tree3.addIterative(12);
+  tree3.addIterative(3);
+  tree3.addIterative(7);
+  tree3.addIterative(2);
+  tree3.addIterative(6);
+  std::cout << "Are first tree and third same? ";
+  if(tree.sameTrees(tree3)) std::cout << "Yes";
+  else std::cout << "No";
   return 0;
 }
 
