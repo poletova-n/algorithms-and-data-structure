5c732bf223ed3b759ab637202b12c29ccd503406
anastasiavdovina <79060271+anastasiavdovina@users.noreply.github.com>
2021-03-30 16:45:33 +0300

    Create main.cpp

---
+++ lab4/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include "binarySearchTree.h"
+
+int main()
+{
+  BinarySearchTree<int> tree;
+  tree.insert(5);
+  tree.insert(7);
+  tree.insert(3);
+  tree.insert(4);
+  tree.print(std::cout);
+  std::cout << "\n";
+  tree.deleteKey(3);
+  tree.print(std::cout);
+  std::cout << "\n";
+  tree.insert(2);
+  tree.insert(10);
+  tree.insert(8);
+  tree.insert(17);
+  tree.print(std::cout);
+  std::cout << "\n";
+  tree.deleteKey(10);
+  tree.print(std::cout);
+  std::cout << "\n";
+  std::cout << "The number of Nodes is: ";
+  std::cout << tree.getCount();
+  std::cout << "\n";
+  std::cout << "The height is: ";
+  std::cout << tree.getHeight();
+  std::cout << "\n";
+  return 0;
+}
