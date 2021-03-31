489f4c5d0a57ef9b227670e41e4134b09d6ec9d8
Ньян Фам-Ван <73636699+famv2000@users.noreply.github.com>
2021-03-31 21:03:31 +0300

    evolved to be better

--- Lab2/stackArray.h
+++ Lab2/stackArray.h
@@ -1,140 +1,91 @@
 #ifndef STACK_ARRAY_H
 #define STACK_ARRAY_H
 
-#include<iostream>
-#include"my_exception.h"
-#include"stack.h"
+#include <iostream>
+#include "myException.h"
 
 template <class T>
-class StackArray :public Stack<T>
+class StackArray
 {
 public:
-	StackArray(size_t size) :
-		top_(-1),
-		size_(size)
-	{
-		try
-		{
-			if (size_ <= 0 || size_ >= 10000)
-			{
-				throw WrongStackSize();
-			}
-			array_ = new T[size_];
-		}
-		catch (WrongStackSize& exp)
-		{
-			std::cout << exp.what() << '\n';
-			exit(0);
-		}
-	}
-	StackArray(const StackArray& src) :
-		top_(src.top_),
-		size_(src.size_)
-	{
-		try
-		{
-			if (size_ <= 0 || size_ >= 10000)
-			{
-				throw WrongStackSize();
-			}
-			array_ = new T[size_];
-		}
-		catch (WrongStackSize& exp)
-		{
-			std::cout << exp.what() << '\n';
-			exit(0);
-		}
-		for (size_t i = 0; i <= src.top_; ++i)
-		{
-			array_[i] = src.array_[i];
-		}
-	}
-	StackArray(const StackArray&& src) :
-		top_(src.top_),
-		size_(src.size_)
-	{
-		try
-		{
-			if (size_ <= 0 || size_ >= 10000)
-			{
-				throw WrongStackSize();
-			}
-			array_ = new T[size_];
-		}
-		catch (WrongStackSize& exp)
-		{
-			std::cout << exp.what() << '\n';
-			exit(0);
-		}
-		for (size_t i = 0; i <= src.top_; ++i)
-		{
-			array_[i] = src.array_[i];
-		}
-		src.top_ = 0;
-		src.size_ = 0;
-		delete[] src.array_;
-	}
+	StackArray(size_t);
+	StackArray(const StackArray<T>&);
+	StackArray(StackArray<T>&&);
 	~StackArray()
 	{
 		delete[] array_;
 	}
-	StackArray<T>& operator=(const StackArray<T>& src);
+
+	StackArray<T>& operator=(const StackArray<T>& src) = delete;
 	StackArray<T>& operator=(StackArray<T>&& src);
-	void push(const T& c) override;
-	T pop() override;
-	bool isEmpty() override;
+	void push(const T& c);
+	T pop();
+	bool isEmpty();
 private:
 	T* array_;
 	size_t top_;
 	size_t size_;
 };
 
-template <class T>
-StackArray<T>& StackArray<T>::operator=(const StackArray<T>& src)
+template<class T>
+StackArray<T>::StackArray(size_t size):
+	top_(0)
 {
-	top_ = src.top_;
-	if (size_ != src.size_)
+	try
 	{
-		size_ = src.size_;
-		try
+		if (size == 0 || size >= 10000)
 		{
-			if (size_ <= 0 || size_ >= 10000)
-			{
-				throw WrongStackSize();
-			}
-			array_ = new T[size_];
+			throw WrongStackSize();
 		}
-		catch (WrongStackSize& exp)
-		{
-			std::cout << exp.what() << '\n';
-			exit(0);
-		}
+		array_ = new T[size];
+		size_ = size;
 	}
+	catch (WrongStackSize& exp)
+	{
+		std::cout << exp.what() << '\n';
+		exit(0);
+	}
+}
+
+template<class T>
+StackArray<T>::StackArray(const StackArray<T>& src)
+{
+	array_ = new T[src.size_];
+	top_ = src.top_;
+	size_ = src.size_;
 	for (size_t i = 0; i <= src.top_; ++i)
 	{
 		array_[i] = src.array_[i];
 	}
 }
+
+
+template<class T>
+StackArray<T>::StackArray(StackArray<T>&& src)
+{
+	array_ = new T[src.size_];
+	size_ = src.size_;
+	top_ = src.top_;
+	for (size_t i = 0; i <= src.top_; ++i)
+	{
+		array_[i] = src.array_[i];
+	}
+	src.top_ = 0;
+	src.size_ = 0;
+}
+
 template <class T>
 StackArray<T>& StackArray<T>::operator=(StackArray<T>&& src)
 {
-	top_ = src.top_;
+	if (this == &src)
+	{
+		return *this;
+	}
 	if (size_ != src.size_)
 	{
+		array_ = new T[src.size_];
 		size_ = src.size_;
-		try
-		{
-			if (size_ <= 0 || size_ >= 10000)
-			{
-				throw WrongStackSize();
-			}
-			array_ = new T[size_];
-		}
-		catch (WrongStackSize& exp)
-		{
-			std::cout << exp.what() << '\n';
-			exit(0);
-		}
+		top_ = src.top_;
 	}
 	for (size_t i = 0; i <= src.top_; ++i)
 	{
@@ -142,37 +93,37 @@ StackArray<T>& StackArray<T>::operator=(StackArray<T>&& src)
 	}
 	src.top_ = 0;
 	src.size_ = 0;
-	delete[] src.array_;
+	return *this;
 }
 template <class T>
 void StackArray<T>::push(const T& c)
 {
-	if (top_ == size_ - 1)
+	if (top_ == size_)
 	{
 		throw StackOverflow();
 	}
 	else
 	{
-		array_[++top_] = c;
+		array_[++top_ - 1] = c;
 	}
 }
 template <class T>
 T StackArray<T>::pop()
 {
-	if (top_ == -1)
+	if (top_ == 0)
 	{
 		throw StackUnderflow();
 	}
 	else
 	{
-		return array_[top_--];
+		return array_[--top_];
 	}
 }
 
 template <class T>
 bool StackArray<T>::isEmpty()
 {
-	return(top_ == -1);
+	return(top_ == 0);
 }
 
 #endif 
