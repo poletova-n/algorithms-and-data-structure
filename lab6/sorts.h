eef20a9a7ee4cb62f37656f58401386ce18b18ee
aosandy <73636600+aosandy@users.noreply.github.com>
2021-04-28 02:13:31 +0300

    Complete sorts.h

--- lab6/sorts.h
+++ lab6/sorts.h
@@ -1,8 +1,10 @@
 #ifndef LAB6_SORTS_H
 #define LAB6_SORTS_H
 
+int binarySearch(int* array, int left, int right, int value);
 void doInsertSort(int* array, int size);
 void doCountSort(int* array, int size);
+void siftHeap(int* array, int size, int root);
 void doPyramidSort(int* array, int size);
 
 #endif
